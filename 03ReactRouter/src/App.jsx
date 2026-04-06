dd6e8ebfb597a2542314efd13d3f1967b45e95cd
sanathecoder <sanasana33377737@gmail.com>
2026-04-06 16:35:04 +0500

    Add Pages

--- 03ReactRouter/src/App.jsx
+++ 03ReactRouter/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   <h1 className='bg-green-600'>React Router</h1>
+   <Header/>
+   <Home/>
+   <Footer/>
    </>
   )
 }
