f29ae0e768c28b4696fd01c080f7f33bf8336cd8
sanathecoder <sanasana33377737@gmail.com>
2026-04-09 15:03:35 +0500

    final

--- 03ReactRouter/src/components/Github/Github.jsx
+++ 03ReactRouter/src/components/Github/Github.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data,setData] = useState([])
-    useEffect(()=>{
-        fetch('https://api.github.com/users/sanathecoder')
-        .then(response=> response.json())
-        .then(data=>{
-            console.log(data)
-            setData(data)
-        })
-    },[])
+
+  const data = useLoaderData()
+  // const [data,setData] = useState([])
+  // useEffect(()=>{
+  //     fetch('https://api.github.com/users/sanathecoder')
+  //     .then(response=> response.json())
+  //     .then(data=>{
+  //         console.log(data)
+  //         setData(data)
+  //     })
+  // },[])
   return (
-    <div className='text-center'>
-      GitHub Followers : {data.followers}
-      <img src={data.avatar_url} alt=" Git Picture" />
+    <div className='flex justify-center items-center'>
+      <div>      <img className='w-[200px] mr-14' src={data.avatar_url} alt=" Git Picture" />
+      </div >
+      <div className='text-center font-bold '> GitHub Followers : {data.followers}</div>
+
+
     </div>
   )
 }
 
 export default Github
+
+
+export const GitHubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/sanathecoder')
+  return response.json()
+}
