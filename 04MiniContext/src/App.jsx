0d801e339e9880bfe75a0066963c87b226b25ee5
sanathecoder <sanasana33377737@gmail.com>
2026-04-11 16:22:31 +0500

    comment change

--- 04MiniContext/src/App.jsx
+++ 04MiniContext/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
+   <UserContextProvider>
    <h1>API Context</h1>
-   </>
+   </UserContextProvider>
    
   )
 }
---
+++ 04MiniContext/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+function Login() {
+
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text" value={username} onChange={(e)=> setUsername(e.target.value)} placeholder='username' />
+        <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='password' />
+        <button onClick={handleSubmit}>Submit</button>
+      
+    </div>
+  )
+}
+
+export default Login
