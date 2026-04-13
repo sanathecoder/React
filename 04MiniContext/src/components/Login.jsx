454e9a1710c731ae6f8e0a74fa3f4e7f9f36c0f3
sanathecoder <sanasana33377737@gmail.com>
2026-04-13 23:52:28 +0500

    login

--- 04MiniContext/src/components/Login.jsx
+++ 04MiniContext/src/components/Login.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
 
 function Login() {
 
     const [username,setUsername] = useState("")
     const [password,setPassword] = useState("")
+
+    const {setUser} = useContext(useContext) 
+    const handleSubmit = (e)=>{
+      e.preventDefault()
+      setUser({username,password})
+
+    }
 
   return (
     <div>
